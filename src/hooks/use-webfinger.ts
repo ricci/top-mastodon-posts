62b0cbbaed681495aa2d91282fce05a46ca0f496
Robert Ricci <ricci@cs.utah.edu>
2024-12-22 15:55:59 -0700

    Add a fallback case for failing webfinger

--- src/hooks/use-webfinger.ts
+++ src/hooks/use-webfinger.ts
@@ -3,9 +3,14 @@ import useSwrImmutable from "swr/immutable";
 const fetcher = (accountName: string) => {
 
     const [username, server] = accountName.split("@");
-    const req = new Request(`https://${server}/.well-known/webfinger?resource=${username}%40${server}`);
-    return fetch(req).then(r => {
-        return "https://" + new URL(r.url).host}, e => { return e});
+    const req = new Request(`https://${server}/.well-known/webfingers?resource=${username}%40${server}`);
+    return fetch(req).then(
+        r => {
+            return "https://" + new URL(r.url).host},
+        e => {
+            // If the webfinger fetch failed, we'll just fall back to the domain the username
+            return "https://" + server;
+        });
 }
 
 export default function useWebfinger({
